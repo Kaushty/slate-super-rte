3e0aae165e32758bdb768533af64b843b7242dfd
kaushty <kaushikshetty4898@gmail.com>
2020-08-23 22:53:06 +0530

    Updates

---
+++ precache-manifest.9d1d373c2e2bf5c4455095e767107a5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "633892a827ae39b557edbfa796e118e6",
+    "url": "/slate-super-rte/index.html"
+  },
+  {
+    "revision": "4fbc6513ac08ea112fac",
+    "url": "/slate-super-rte/static/css/main.cf429900.chunk.css"
+  },
+  {
+    "revision": "5130b5a1462be6c5dffc",
+    "url": "/slate-super-rte/static/js/2.85593c40.chunk.js"
+  },
+  {
+    "revision": "5c1615e7d4ba5641752a11ee63eef588",
+    "url": "/slate-super-rte/static/js/2.85593c40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fbc6513ac08ea112fac",
+    "url": "/slate-super-rte/static/js/main.31b97424.chunk.js"
+  },
+  {
+    "revision": "4975e00e47f9d16f775f",
+    "url": "/slate-super-rte/static/js/runtime-main.4348b634.js"
+  }
+]);
